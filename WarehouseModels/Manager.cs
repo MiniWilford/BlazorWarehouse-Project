94e104cfd7e816d6eea4157cce52b7770dc7c4e3
Guy-leroc Ossebi <thonerguyleroc1@gmail.com>
2023-11-26 20:53:30 -0500

    aded manager control, manager model and implemeted a table that shows manager's and their title need to add a button to view employees that are under that manager and a add manager button.

---
+++ WarehouseModels/Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseModels
+{
+    
+    public class Manager
+    {
+        public int ManagerId { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [MaxLength(50)]
+        public string LastName { get; set; }
+        [MaxLength(50)]
+        public string? Title { get; set; }
+    }
+}
