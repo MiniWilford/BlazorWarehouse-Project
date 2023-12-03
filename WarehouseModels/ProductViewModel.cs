121eb8a3476befbd0e35f5a828aff3a13751f2aa
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2023-12-03 01:51:41 -0500

    Added products page / razor components / pruduct viewmodel & product CRUD API

---
+++ WarehouseModels/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseModels
+{
+	public class ProductViewModel
+	{
+		public int ProductId { get; set; }
+		[MaxLength(50)]
+		public string? ProductName { get; set; }
+		public float ProductPrice { get; set; }
+	}
+}
